36e8463d03dd9a6903105ad1b050c3faa0ab4eb2
Robin8342 <88617511+Robin8342@users.noreply.github.com>
2021-08-21 23:16:16 +0900

    2021.08.21 Saturday Python Image Read & Change Image Color & Change Image Size

---
+++ Opencv/ImageChangeColor.py
@@ -0,0 +1,35 @@
+import cv2
+
+Img_color = cv2.imread('./Image/Coin.jpg',cv2.IMREAD_COLOR) #Image Read
+Img_gray = cv2.cvtColor(Img_color, cv2.COLOR_BGR2GRAY)  #open image change Color : "GRAY"
+
+
+""" #Image each Show
+#cv2.namedWindow('Show Image')
+#cv2.imshow('Show Image', img_color)    #show Image-> Program Title name, img_color -> address image open
+#cv2.waitKey(0) #keybord wait
+
+
+#cv2.imshow('Show Gray Image',img_gray)
+#cv2.waitKey(0)
+"""
+
+
+#Image Resize Absolute Value
+ColorImageSize = cv2.resize(Img_color,dsize=(640,480), interpolation=cv2.INTER_AREA)
+GrayImageSize = cv2.resize(Img_gray,dsize=(800,800), interpolation=cv2.INTER_AREA)
+
+
+"""
+#Image Resize Relative Value
+ColorImageSize = cv2.resize(Img_color,dsize=(0,0), fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
+GrayImageSize = cv2.resize(Img_gray,dsize=(0,0), fx=0.6, fy=0.6, interpolation=cv2.INTER_LINEAR)
+"""
+
+while True:
+    cv2.imshow('Show Color Image',ColorImageSize)
+    cv2.imshow('Show Gray Image',GrayImageSize)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows() #all windows cash memory destroy
