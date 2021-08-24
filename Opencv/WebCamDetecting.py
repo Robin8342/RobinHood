fa71be5727ab9bc417d0af13630f89aff84d8362
Robin8342 <88617511+Robin8342@users.noreply.github.com>
2021-08-24 23:05:32 +0900

    2021.08.24 Tuesday Python Opencv object detection

--- Opencv/WebCamDetecting.py
+++ Opencv/WebCamDetecting.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
-
+#환경에 따라 다르지만 이 코드는 파란색이 잘 나오게 된다.
+#특히
 
 hsv = 0
 lower_blue1 = 0
@@ -10,9 +11,13 @@ upper_blue2 = 0
 lower_blue3 = 0
 upper_blue3 = 0
 
+#트랙바 생성시 필요로 하는 함수
+def nothing(x):
+    pass
+
 
 def mouse_callback(event, x, y, flags, param):
-    global hsv, lower_blue1, upper_blue1, lower_blue2, upper_blue2, lower_blue3, upper_blue3
+    global hsv, lower_blue1, upper_blue1, lower_blue2, upper_blue2, lower_blue3, upper_blue3, threshold
 
     # 마우스 왼쪽 버튼 누를시 위치에 있는 픽셀값을 읽어와서 HSV로 변환합니다.
     if event == cv.EVENT_LBUTTONDOWN:
@@ -23,35 +28,37 @@ def mouse_callback(event, x, y, flags, param):
         hsv = cv.cvtColor(one_pixel, cv.COLOR_BGR2HSV)
         hsv = hsv[0][0]
 
+        threshold = cv.getTrackbarPos('threshold','img_result')
+
         # HSV 색공간에서 마우스 클릭으로 얻은 픽셀값과 유사한 필셀값의 범위를 정합니다.
         if hsv[0] < 10:
             print("case1")
-            lower_blue1 = np.array([hsv[0]-10+180, 30, 30])
+            lower_blue1 = np.array([hsv[0]-10+180, threshold, threshold])
             upper_blue1 = np.array([180, 255, 255])
-            lower_blue2 = np.array([0, 30, 30])
+            lower_blue2 = np.array([0, threshold, threshold])
             upper_blue2 = np.array([hsv[0], 255, 255])
-            lower_blue3 = np.array([hsv[0], 30, 30])
+            lower_blue3 = np.array([hsv[0], threshold, threshold])
             upper_blue3 = np.array([hsv[0]+10, 255, 255])
             #     print(i-10+180, 180, 0, i)
             #     print(i, i+10)
 
         elif hsv[0] > 170:
             print("case2")
-            lower_blue1 = np.array([hsv[0], 30, 30])
+            lower_blue1 = np.array([hsv[0], threshold, threshold])
             upper_blue1 = np.array([180, 255, 255])
-            lower_blue2 = np.array([0, 30, 30])
+            lower_blue2 = np.array([0, threshold, threshold])
             upper_blue2 = np.array([hsv[0]+10-180, 255, 255])
-            lower_blue3 = np.array([hsv[0]-10, 30, 30])
+            lower_blue3 = np.array([hsv[0]-10, threshold, threshold])
             upper_blue3 = np.array([hsv[0], 255, 255])
             #     print(i, 180, 0, i+10-180)
             #     print(i-10, i)
         else:
             print("case3")
-            lower_blue1 = np.array([hsv[0], 30, 30])
+            lower_blue1 = np.array([hsv[0], threshold, threshold])
             upper_blue1 = np.array([hsv[0]+10, 255, 255])
-            lower_blue2 = np.array([hsv[0]-10, 30, 30])
+            lower_blue2 = np.array([hsv[0]-10, threshold, threshold])
             upper_blue2 = np.array([hsv[0], 255, 255])
-            lower_blue3 = np.array([hsv[0]-10, 30, 30])
+            lower_blue3 = np.array([hsv[0]-10, threshold, threshold])
             upper_blue3 = np.array([hsv[0], 255, 255])
             #     print(i, i+10)
             #     print(i-10, i)
@@ -65,10 +72,17 @@ def mouse_callback(event, x, y, flags, param):
 cv.namedWindow('img_color')
 cv.setMouseCallback('img_color', mouse_callback)
 
+cv.namedWindow('img_result')
+cv.createTrackbar('threshold','img_result',0,255,nothing)
+cv.setTrackbarPos('threshold','img_result',30)
+
+
+cap = cv.VideoCapture(0)
 
 
 while(True):
-    img_color = cv.imread('2.jpg')
+    #img_color = cv.imread('./Image/Coin.jpg')
+    ret,img_color = cap.read()
     height, width = img_color.shape[:2]
     img_color = cv.resize(img_color, (width, height), interpolation=cv.INTER_AREA)
 
@@ -82,8 +96,30 @@ while(True):
     img_mask = img_mask1 | img_mask2 | img_mask3
 
 
+    #노이즈 제거
+    kernel = np.ones((11,11), np.uint8)
+    #검출 된 물체의 검은 구멍들을 메우는 식 (조명의 밝거나 어두운 경우 생김)
+    img_mask = cv.morphologyEx(img_mask,cv.MORPH_OPEN,kernel)
+    img_mask = cv.morphologyEx(img_mask,cv.MORPH_CLOSE,kernel)
+
     # 마스크 이미지로 원본 이미지에서 범위값에 해당되는 영상 부분을 획득합니다.
     img_result = cv.bitwise_and(img_color, img_color, mask=img_mask)
+
+    numOfLabels, img_label, stats, centroids = cv.connectedComponentsWithStats(img_mask)
+
+    for idx, centroid in enumerate(centroids):
+        if stats[idx][0] == 0 and stats[idx][1] == 0:
+            continue
+
+        if np.any(np.isnan(centroid)):
+            continue
+
+        x,y,width,height,area = stats[idx]
+        centerX,centerY = int(centroid[0]), int(centroid[1])
+
+        if area > 50:
+            cv.circle(img_color, (centerX, centerY), 10, (0,0,255), 10)
+            cv.rectangle(img_color, (x,y), (x+width, y+height), (0,0,255))
 
 
     cv.imshow('img_color', img_color)
